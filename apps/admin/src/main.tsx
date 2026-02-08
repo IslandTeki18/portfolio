178434e8c17c63018cc46280a740a45e60f9eb98
Landon McKell <landon.roney7923@gmail.com>
2026-02-08 08:06:59 -0700

    Convex

--- apps/admin/src/main.tsx
+++ apps/admin/src/main.tsx
@@ -1,13 +1,17 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
+import { ConvexProvider } from "convex/react";
+import { convex } from "./lib/convex";
 import "./index.css";
 import App from "./App";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <BrowserRouter>
-      <App />
+      <ConvexProvider client={convex}>
+        <App />
+      </ConvexProvider>
     </BrowserRouter>
   </StrictMode>,
 );
